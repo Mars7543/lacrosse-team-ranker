a7df3e1f0f0524f35c9ec1d9e95b3cbf46f139a0
Nick8727 <niclev20@bergen.org>
2018-10-31 15:55:04 -0400

    made the print_graph function for testing purposes. graph seems to be correct as of now.

--- graph.py
+++ graph.py
@@ -8,16 +8,25 @@ class Graph(object):
         wb = load_workbook(filename=file, read_only=True)
         ws = wb['Sheet1']
         team_ids = load_teams(file)
+        self.id_to_team_name = self.__get_id_to_team__(team_ids)
 
         for row in ws.iter_rows(min_row=2):
             teamA = row[0].value  # Name of Team A
-            teamB = row[3].value # Name of Team B
+            teamB = row[3].value  # Name of Team B
 
-            scoreA = row[1].value # Score of Team A
-            scoreB = row[2].value # Score of Team B
+            scoreA = row[1].value  # Score of Team A
+            scoreB = row[2].value  # Score of Team B
 
+            self.__connect_nodes__(team_ids.get(teamA), team_ids.get(teamB), scoreA, scoreB)
 
-    def connect_nodes(self, nodeA, nodeB, scoreA, scoreB):
+    def __get_id_to_team__(self, team_ids):
+        id_to_team = {}
+        for key, value in team_ids.items():
+            id_to_team[value] = key
+
+        return id_to_team
+
+    def __connect_nodes__(self, nodeA, nodeB, scoreA, scoreB):
         key_list = self.adj.keys()
 
         if nodeA in key_list:
@@ -31,3 +40,17 @@ class Graph(object):
 
         else:
             self.adj[nodeB] = {nodeA : scoreB}
+
+    #  Prints the graph for testing purposes.
+    def print_graph(self):
+        for key, value in self.adj.items():
+            print("%-20s" % self.id_to_team_name.get(key))
+
+            for key2, value2 in value.items():
+                print("%-20sTeam %10s:%10s" % ("", self.id_to_team_name.get(key2), value2))
+
+    def get_ranking_of(self, teamA):
+        pass
+
+graph = Graph("data.xlsx")
+graph.print_graph()
